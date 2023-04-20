86c38209a8ea3bf2d4e6cfa55590bce2d1284877
fcardenas07 <f.cardenasreyes.94@gmail.com>
2023-04-20 16:41:01 -0400

    merge

--- src/test/java/RegistroTest.java
+++ src/test/java/RegistroTest.java
@@ -1,4 +1,3 @@
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -10,15 +9,15 @@ class RegistroTest {
 
     @BeforeEach
     void setUp() {
-        registro[0] = new String[]{"Juan", "Casado", "30"};
-        registro[1] = new String[]{"Pedro", "Soltero", "15"};
-        registro[2] = new String[]{"Diego", "Casado", "40"};
+        registro[0] = new String[]{"Juan", "casado/a", "30"};
+        registro[1] = new String[]{"Pedro", "soltero/a", "15"};
+        registro[2] = new String[]{"Diego", "casado/a", "40"};
     }
 
 
     @Test
     void obtenerUltimoEspacioTest() {
-        assertEquals(3,Registro.obtenerUltimoEspacio(registro));
+        assertEquals(3, Registro.retornarFilaVacia(registro));
     }
 
     @Test
@@ -30,33 +29,34 @@ class RegistroTest {
     @Test
     void retornarFilaVaciaTest() {
         for (int i = 0; i < Registro.retornarFilaVacia(registro); i++) {
-            assertNotEquals(i,Registro.retornarFilaVacia(registro));
+            assertNotEquals(i, Registro.retornarFilaVacia(registro));
         }
     }
 
     @Test
     void contarPersonasTerceraEdad() {
-    assertEquals(0,Registro.contarPersonasTerceraEdad(registro));
+        assertEquals(0, Registro.contarPersonasTerceraEdad(registro));
 
     }
 
     @Test
     void contarMenoresDeEdad() {
-        assertEquals(1,Registro.contarMenoresDeEdad(registro));
+        assertEquals(1, Registro.contarMenoresDeEdad(registro));
     }
 
     @Test
     void contarMayoresDeEdad() {
-        assertEquals(2,Registro.contarMayoresDeEdad(registro));
+        assertEquals(2, Registro.contarMayoresDeEdad(registro));
     }
 
     @Test
-    void contarCasadosTest(){
-        assertEquals(2,Registro.contarCasados(registro));
+    void contarCasadosTest() {
+        assertEquals(2, Registro.contarCasados(registro));
     }
 
-    void contarSolterosTest(){
-        assertEquals(1,Registro.contarSolteros(registro));
+    @Test
+    void contarSolterosTest() {
+        assertEquals(1, Registro.contarSolteros(registro));
     }
 
 }
